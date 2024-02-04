4d916acbf0f01c8aa98f3b38bfa5e58009896115
Sina Salimirad <138554159+sinasalimirad@users.noreply.github.com>
2024-02-04 15:19:14 +0330

    completed

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 let submit = document.querySelector('[submit]'),
-    input = document.querySelector('[input]');
+    input = document.querySelector('[input]'),
+    errors = document.querySelectorAll('[error]');
 
 submit.addEventListener('click', () => {
-    if (input.value.length < 8) {
-        alert('smaller than 8')
+    let check = /@gmail.com/;
+    if (input.value.match(check) && input.value.length >= 18) {
+        alert('Sent successful');
     } else {
-        alert('bigger than 8')
-    }  
+        for (let i = 0; i < errors.length; ++i) {
+            errors[i].classList.add('show');
+        }
+
+        input.classList.add('error');
+
+        input.value = '';
+    }
+});
+
+input.addEventListener('click', () => {
+    for (let i = 0; i < errors.length; ++i) {
+        errors[i].classList.remove('show');
+    }
+
+    input.classList.remove('error');
 });
